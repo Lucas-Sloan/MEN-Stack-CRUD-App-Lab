537933a2d94db1e7e2366f911d5f76c3540ee2e3
Lucas-Sloan <Lucasg.sloan@gmail.com>
2024-07-25 15:29:33 -0500

    Added connection to MongoDB

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
+const dotenv = require("dotenv"); // require package
+dotenv.config(); // Loads the environment variables from .env file
 const express = require("express");
-
+const mongoose = require("mongoose"); // require package
 const app = express();
+
+mongoose.connect(process.env.MONGODB_URI);
+// log connection status to terminal on start
+mongoose.connection.on("connected", () => {
+  console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
+});
+
 
 app.listen(3000, () => {
   console.log("Listening on port 3000");
 });
 
 app.get("/", async (req, res) => {
-    res.send("hello, friend!");
+    res.render("index.ejs");
   });
+  
