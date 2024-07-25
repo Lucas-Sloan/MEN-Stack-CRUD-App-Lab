55d63869a0eb55b943342c41292be9ee7d6b6645
Lucas-Sloan <Lucasg.sloan@gmail.com>
2024-07-25 15:41:56 -0500

    Built the new team page

--- server.js
+++ server.js
@@ -12,11 +12,16 @@ mongoose.connection.on("connected", () => {
 
 const Team = require("./models/teams.js");
 
+app.get("/", async (req, res) => {
+    res.render("index.ejs");
+  });
+
+app.get("/teams/new", (req, res) => {
+    res.render("teams/new.ejs");
+  });
+  
 app.listen(3000, () => {
   console.log("Listening on port 3000");
 });
 
-app.get("/", async (req, res) => {
-    res.render("index.ejs");
-  });
-  
+
